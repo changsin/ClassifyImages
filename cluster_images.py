857936cd6bcdb30925f017b95e2694e5b0455abc
Changsin <changsin@hotmail.com>
2021-08-11 12:18:30 +0900

    Reduced the memory footprint so that it can run in my PC.

--- cluster_images.py
+++ cluster_images.py
@@ -14,11 +14,13 @@ from sklearn.decomposition import PCA
 """
 """
 
+IMAGE_SIZE = 320
+
 def glob_files(folder, file_type='*'):
     search_string = os.path.join(folder, file_type)
     files = glob.glob(search_string)
 
-    # print('searching ', path)
+    print('Searching ', search_string)
     paths = []
     for f in files:
       if os.path.isdir(f):
@@ -33,68 +35,36 @@ def glob_files(folder, file_type='*'):
 
     return paths
 
-def load_images(path, file_type="*"):
-  files = glob_files(path, file_type)
 
-  # print(files)
-  X_data = []
-  for file in files:
-    image = cv2.imread(file)
-    # reduce image sizes
-    x = cv2.resize(image, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
-    x = np.expand_dims(x, axis=0)
-    if len(X_data) > 0:
-        X_data = np.concatenate((X_data, x))
-    else:
-        X_data = x
+def get_feature_maps(path, file_type="*.png"):
+    def _to_feature_maps(X):
+        #Convert to VGG input format
+        X = preprocess_input(X)
 
-    # X_data.append(image)
-  X_data = np.array(X_data)
-  print("Loaded {}".format(X_data.shape))
-  return X_data
+        #include_top=False == not getting VGG16 last 3 layers
+        model = VGG16(weights="imagenet", include_top=False)
+        #Get features
+        features = model.predict(X)
+        print(features.shape)
 
-def find_duplicates(X_train_pca, threshold=0.1):
-    # Calculate distances of all points
-    distances = cdist(X_train_pca, X_train_pca)
+        return features
 
-    # Find duplicates (very similar images)
-    # dupes = np.array([np.where(distances[id] < 1) for id in range(distances.shape[0])]).reshape(-1)
-    dupes = [np.array(np.where(distances[id] < threshold)).reshape(-1).tolist() \
-            for id in range(distances.shape[0])]
+    files = glob_files(path, file_type)
 
-    to_remove = set()
-    for d in dupes:
-        if len(d) > 1:
-            for id in range(1, len(d)):
-                to_remove.add(d[id])
-    logger.info("Found {} duplicates".format(len(to_remove)))
-    return to_remove
+    feature_maps = []
+    for file in files:
+        print(file)
+        image = cv2.imread(file)
+        image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+        fm = _to_feature_maps(np.array([image]))
+        feature_maps.append(fm)
 
-#Calculate similar matrics
-def cosine_similarity(ratings):
-    sim = ratings.dot(ratings.T)
-    if not isinstance(sim,np.ndarray):
-        sim = sim.toarray()
-    norms = np.array([np.sqrt(np.diagonal(sim))])
-    return (sim/norms/norms.T)
-
-
-def get_feature_maps(X):
-    #Convert to VGG input format
-    X = preprocess_input(X)
-
-    #include_top=False == not getting VGG16 last 3 layers
-    model = VGG16(weights="imagenet", include_top=False)
-    # model = VGG16(weights = None, include_top=False)
-    #Get features
-    features = model.predict(X)
-    print(features.shape)
-
-    return features
+    return np.array(feature_maps)
 
 
 def get_pca_reduced(X_features, dimensions=2):
   X_features_flatten = X_features.reshape(X_features.shape[0], -1)
+  print("Flattened shape: ", X_features_flatten.shape)
   pca = PCA(dimensions)
 
   X_features_pca_reduced = pca.fit_transform(X_features_flatten)
@@ -119,20 +89,20 @@ def to_cluster_idx(cluster_labels, bins):
         cluster_dict[cluster_id] = np.where(cluster_labels == cluster_id)[0]
     return cluster_dict
 
-def cluster_images(folder):
-    X_data = load_images(folder, "*.png")
-
-    # get feature maps
-    X_features = get_feature_maps(X_data)
+def cluster_images(folder, file_type="*.png"):
+    X_fm = get_feature_maps(folder, file_type=file_type)
+    print("####", X_fm.shape)
 
     # normalize
-    X_fm_normalized = preprocessing.normalize(X_features.reshape(len(X_features), -1))
+    X_fm_normalized = preprocessing.normalize(X_fm.reshape(len(X_fm), -1))
 
-    # dimensionality reduction through PCA
+    print(X_fm_normalized.shape)
+    # # dimensionality reduction through PCA
     X_reduced, pca = get_pca_reduced(X_fm_normalized, dimensions=2)
 
-    # cluster
+    # # cluster
     X_clusters, kmeans = get_clusters(X_reduced, 2)
+    # X_clusters, kmeans = get_clusters(X_fm_normalized, 2)
 
     # get the image ids of each cluster
     cluster_idx = to_cluster_idx(X_clusters.labels_, [0, 1])
@@ -140,6 +110,7 @@ def cluster_images(folder):
     # keep the cluster centers
     print(kmeans.cluster_centers_)
     print(cluster_idx)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
