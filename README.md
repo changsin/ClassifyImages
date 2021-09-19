451a4e4d29bc4d7ab363d13098d01652a51f6bef
Changsin <changsin@hotmail.com>
2021-09-19 23:33:54 +0900

    Refactored convertor and updaetd README

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ There are two modes you can run the script: cluster or classify
  The centroid values are automatically saved in centroids.json file.
 2. In 'classify' mode, the centroid values are read from the json file and used to classify new images. 
  If the similarity measures (distance between two images) are too high, a warning message will be shown.
+
+
+## convert_labels.py
+Converts label files from one format to another.
+Currently, supported formats are:
+
+- CVAT xml to EdgeImpulse
+- Kaggle xml to EdgeImpulse
+
+Example:
+```
+python .\convert_labels.py -mode convert -path .\SkNetworks_CarDashboard_21036\01.rawData\2\BMW\BMW_day_0_1.xml -for
+mat_in cvat_xml
+```
--- convert_labels.py
+++ convert_labels.py
@@ -108,6 +108,7 @@ class Convertor:
     def parse(self, path, file_type='*'):
         pass
 
+
 class KaggleXmlConvertor(Convertor):
     def parse(self, folder, file_type='*'):
         def _parse_file(filename):
@@ -140,6 +141,7 @@ class KaggleXmlConvertor(Convertor):
 
         return np.array(image_labels)
 
+
 class CVATXmlConvertor(Convertor):
     def parse(self, filename, file_type='*'):
         image_labels = []
@@ -161,10 +163,10 @@ class CVATXmlConvertor(Convertor):
 
                 alertwarning = box.attrib['label']
                 wtype = box.xpath('attribute[@name="name"]')[0].text
-                daynight = box.xpath('attribute[@name="daynight"]')[0].text
-                visibility = int(box.xpath('attribute[@name="visibility"]')[0].text)
+                # daynight = box.xpath('attribute[@name="daynight"]')[0].text
+                # visibility = int(box.xpath('attribute[@name="visibility"]')[0].text)
 
-                box = wtype, alertwarning, daynight, visibility, xtl, ytl, xbr, ybr
+                box = "{}@{}".format(alertwarning, wtype), xtl, ytl, xbr, ybr
 
                 boxes.append(box)
 
@@ -240,8 +242,7 @@ class EdgeImpulseLabels:
         return to_return
 
 
-def kaggle_xml2edge_impulse(path):
-    convertor = KaggleXmlConvertor()
+def to_edge_impulse(path, convertor):
     parsed = convertor.parse(path)
 
     image_labels = {}
@@ -263,54 +264,15 @@ def kaggle_xml2edge_impulse(path):
 
     eilabels = EdgeImpulseLabels(image_labels)
 
-    # print(json.dumps(eilabels.__dict__, indent='  '))
     print(json.dumps(eilabels.to_json(),  separators=(',', ':')))
-    # print(json.dumps(eilabels, indent='  '))
     to_file(".\\" + os.path.basename(path) + '.json', json.dumps(eilabels.to_json(), separators=(',', ':')))
-
-
-def cvat_xml2edge_impulse(path):
-    convertor = CVATXmlConvertor()
-    parsed = convertor.parse(path)
-
-    image_labels = {}
-
-    for image_info in parsed:
-        image_filename = image_info[0]
-
-        labels = []
-        for a in image_info[3]:
-            width = float(a[6]) - float(a[4])
-            height = float(a[7]) - float(a[5])
-            image_label = ImageLabel(a[0], int(float(a[4])), int(float(a[5])),
-                                     int(width), int(height))
-            labels.append(image_label)
-
-        image_labels[image_filename] = labels
-    # nums = [1, 2, 3, 4]
-    # print(json.dumps(nums))
-
-        # print(image_filename, labels[0])
-        # print(image_filename, json.dumps(labels))
-        # print(image_filename, labels[0].__dict__)
-        # image_labels[image_filename] = labels
-        # print(image_labels)
-
-    # print(image_labels)
-
-    eilabels = EdgeImpulseLabels(image_labels)
-
-    # print(json.dumps(eilabels.__dict__, indent='  '))
-    print(json.dumps(eilabels.to_json(),  separators=(',', ':')))
-    # print(json.dumps(eilabels, indent='  '))
-    to_file(".\\" + os.path.basename(path) + '.json', json.dumps(eilabels.to_json(),  separators=(',', ':')))
 
 
 def convert_labels(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
     if from_format == LabelFormat.CVAT_XML:
-        cvat_xml2edge_impulse(args.path)
+        to_edge_impulse(args.path, CVATXmlConvertor())
     elif from_format == LabelFormat.KAGGLE_XML:
-        kaggle_xml2edge_impulse(args.path)
+        to_edge_impulse(args.path, KaggleXmlConvertor())
     else:
         print('Unsupported format {}'.format(from_format))
 
