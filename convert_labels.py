6b039ad81b4c4542685367db02935b985f634c88
Changsin <changsin@hotmail.com>
2021-09-19 22:56:59 +0900

    Refactored convertor

--- convert_labels.py
+++ convert_labels.py
@@ -32,6 +32,25 @@ class Mode(Enum):
             return s
 
 
+class LabelFormat(Enum):
+    CVAT_XML = 'cvat_xml'
+    KAGGLE_XML = 'kaggle_xml'
+    EDGE_IMPULSE = 'edge_impulse'
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def argparse(s):
+        try:
+            return LabelFormat[s.upper()]
+        except KeyError:
+            return s
+
+
 def glob_files(folder, file_type='*'):
     search_string = os.path.join(folder, file_type)
     files = glob.glob(search_string)
@@ -85,36 +104,73 @@ def from_file(path):
     return json.load(file)
 
 
-def parse_xml(filename):
-    image_labels = []
+class Convertor:
+    def parse(self, path, file_type='*'):
+        pass
 
-    tree = etree.parse(filename)
-    for image in tree.xpath('image'):
-        # print(image.attrib['name'])
-        name = image.attrib['name']
-        width = int(image.attrib['width'])
-        height = int(image.attrib['height'])
+class KaggleXmlConvertor(Convertor):
+    def parse(self, folder, file_type='*'):
+        def _parse_file(filename):
+            width, height = 0, 0
+            xmin, ymin, xmax, ymax = 0, 0, 0, 0
 
-        boxes = []
+            tree = etree.parse(filename)
+            for dim in tree.xpath("size"):
+                width = int(dim.xpath("width")[0].text)
+                height = int(dim.xpath("height")[0].text)
 
-        for box in image.xpath('box'):
-            xtl = float(box.attrib['xtl'])
-            ytl = float(box.attrib['ytl'])
-            xbr = float(box.attrib['xbr'])
-            ybr = float(box.attrib['ybr'])
+            for dim in tree.xpath("object/bndbox"):
+                xmin = int(dim.xpath("xmin")[0].text)
+                ymin = int(dim.xpath("ymin")[0].text)
+                xmax = int(dim.xpath("xmax")[0].text)
+                ymax = int(dim.xpath("ymax")[0].text)
 
-            alertwarning = box.attrib['label']
-            wtype = box.xpath('attribute[@name="name"]')[0].text
-            daynight = box.xpath('attribute[@name="daynight"]')[0].text
-            visibility = int(box.xpath('attribute[@name="visibility"]')[0].text)
+            return [width, height,
+                    [["license", xmin, ymin, xmax, ymax]]]
 
-            box = wtype, alertwarning, daynight, visibility, xtl, ytl, xbr, ybr
+        files = glob_files(folder, file_type=file_type)
 
-            boxes.append(box)
+        image_labels = []
 
-        image_labels.append([name, width, height, np.array(boxes)])
+        for file in files:
+            width, height, image_label = _parse_file(file)
+            basename = os.path.basename(file)
 
-    return np.array(image_labels)
+            image_labels.append([basename, width, height, np.array(image_label)])
+
+        return np.array(image_labels)
+
+class CVATXmlConvertor(Convertor):
+    def parse(self, filename, file_type='*'):
+        image_labels = []
+
+        tree = etree.parse(filename)
+        for image in tree.xpath('image'):
+            # print(image.attrib['name'])
+            name = image.attrib['name']
+            width = int(image.attrib['width'])
+            height = int(image.attrib['height'])
+
+            boxes = []
+
+            for box in image.xpath('box'):
+                xtl = float(box.attrib['xtl'])
+                ytl = float(box.attrib['ytl'])
+                xbr = float(box.attrib['xbr'])
+                ybr = float(box.attrib['ybr'])
+
+                alertwarning = box.attrib['label']
+                wtype = box.xpath('attribute[@name="name"]')[0].text
+                daynight = box.xpath('attribute[@name="daynight"]')[0].text
+                visibility = int(box.xpath('attribute[@name="visibility"]')[0].text)
+
+                box = wtype, alertwarning, daynight, visibility, xtl, ytl, xbr, ybr
+
+                boxes.append(box)
+
+            image_labels.append([name, width, height, np.array(boxes)])
+
+        return np.array(image_labels)
 
 
 def default(obj):
@@ -184,8 +240,38 @@ class EdgeImpulseLabels:
         return to_return
 
 
-def xml2pascalvoc(filename):
-    parsed = parse_xml(filename)
+def kaggle_xml2edge_impulse(path):
+    convertor = KaggleXmlConvertor()
+    parsed = convertor.parse(path)
+
+    image_labels = {}
+
+    for image_info in parsed:
+        image_filename = image_info[0]
+
+        print(image_info)
+
+        labels = []
+        for a in image_info[3]:
+            width = float(a[3]) - float(a[1])
+            height = float(a[4]) - float(a[2])
+            image_label = ImageLabel(a[0], int(float(a[1])), int(float(a[2])),
+                                     int(width), int(height))
+            labels.append(image_label)
+
+        image_labels[image_filename] = labels
+
+    eilabels = EdgeImpulseLabels(image_labels)
+
+    # print(json.dumps(eilabels.__dict__, indent='  '))
+    print(json.dumps(eilabels.to_json(),  separators=(',', ':')))
+    # print(json.dumps(eilabels, indent='  '))
+    to_file(".\\" + os.path.basename(path) + '.json', json.dumps(eilabels.to_json(), separators=(',', ':')))
+
+
+def cvat_xml2edge_impulse(path):
+    convertor = CVATXmlConvertor()
+    parsed = convertor.parse(path)
 
     image_labels = {}
 
@@ -215,18 +301,28 @@ def xml2pascalvoc(filename):
     eilabels = EdgeImpulseLabels(image_labels)
 
     # print(json.dumps(eilabels.__dict__, indent='  '))
-    print(json.dumps(eilabels.to_json()))
+    print(json.dumps(eilabels.to_json(),  separators=(',', ':')))
     # print(json.dumps(eilabels, indent='  '))
-    to_file(".\\" + os.path.basename(filename) + '.json', json.dumps(eilabels.to_json()))
+    to_file(".\\" + os.path.basename(path) + '.json', json.dumps(eilabels.to_json(),  separators=(',', ':')))
+
+
+def convert_labels(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
+    if from_format == LabelFormat.CVAT_XML:
+        cvat_xml2edge_impulse(args.path)
+    elif from_format == LabelFormat.KAGGLE_XML:
+        kaggle_xml2edge_impulse(args.path)
+    else:
+        print('Unsupported format {}'.format(from_format))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-mode", action="store", type=Mode.argparse, choices=list(Mode), dest="mode")
+    parser.add_argument("-format_in", action="store", type=LabelFormat.argparse, choices=list(LabelFormat), dest="format_in")
+    # parser.add_argument("-format_out", action="store", type=LabelFormat.argparse, choices=list(LabelFormat), dest="format_out")
     parser.add_argument("-path", action="store", dest="path", type=str)
     parser.add_argument("-path_out", action="store", dest="path_out", type=str)
-    parser.add_argument("-centroids_json", action="store", dest="centroids_json", type=str)
-    parser.add_argument("-threshold", action="store", dest="threshold", type=float, default=80)
 
     args = parser.parse_args()
 
-    xml2pascalvoc(args.path)
+    convert_labels(args.path, args.format_in)
