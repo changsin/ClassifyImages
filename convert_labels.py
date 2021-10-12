6845b11dbe71c9687a567f2aa90a7c055e57f167
Changsin <changsin@hotmail.com>
2021-10-12 16:32:34 +0900

    Updated filter and added dashboard template

--- convert_labels.py
+++ convert_labels.py
@@ -9,6 +9,8 @@ from enum import Enum
 
 import numpy as np
 import random
+import urllib
+
 
 from lxml import etree as ET
 
@@ -527,11 +529,11 @@ def convert_labels(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
 
 
 TO_EXCLUDE = [
-    "alert@Retaining",
-    "alert@Distance",
-    "warning@ABS",
-    "alert@Coolant",
-    "warning@Fuel",
+    # "alert@Retaining",
+    # "alert@Distance",
+    # "warning@ABS",
+    # "alert@Coolant",
+    # "warning@Fuel",
     "warning@Retaining",
     "warning@Steering",
     "alert@EngineOilTemp",
@@ -541,39 +543,84 @@ TO_EXCLUDE = [
     "warning@Washer"
 ]
 
-def filter_files(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
+TOPS = [
+    # Top 5
+    "alert@Seatbelt",
+    "warning@Engine",
+    "alert@Parking",
+    "warning@Tire",
+    "warning@StabilityOn",
+
+    # Top 10
+    "alert@Brake",
+    "warning@StabilityOff",
+    "warning@Brake",
+    "alert@Steering",
+    "warning@Parking",
+
+    # Top 15
+    "alert@Retaining",
+    "alert@Distance",
+    "warning@ABS",
+    "alert@Coolant",
+    "warning@Fuel"
+]
+
+TOP5 = TOPS[:5]
+TOP10 = TOPS[5:10]
+TOP15 = TOPS[10:15]
+
+
+def filter_by_labels(parsed, labels, is_in=True):
     # check for file name dupes
     def _is_dupe(image_info, flist):
         for f in flist:
             if f[0] == image_info[0]:
-                print("Dupe found {}".format(f))
+                # print("Dupe found {}".format(f))
                 return True
         return False
 
-    def filter_by_labels(parsed, labels, is_in=True):
-        filtered = []
-        dupe_count = 0
-        for image_info in parsed:
-            if _is_dupe(image_info, filtered):
-                dupe_count += 1
-                print("Skipping")
-                continue
+    filtered = []
+    dupe_count = 0
+    for image_info in parsed:
+        if _is_dupe(image_info, filtered):
+            dupe_count += 1
+            # print("Skipping")
+            continue
 
-            match_found = False
-            for box in image_info[-1]:
-                if box[0] in labels:
-                    match_found = True
-                    # print("Adding ", image_info[0], label, box)
-                    if is_in:
-                        filtered.append(image_info)
-                        break
+        match_found = False
+        for box in image_info[-1]:
+            if box[0] in labels:
+                match_found = True
+                # print("Adding ", image_info[0], label, box)
+                if is_in:
+                    filtered.append(image_info)
+                    break
 
-            # if it's a negative case, wait till all labels are tried before adding to filtered
-            if not is_in and not match_found:
-                filtered.append(image_info)
+        # if it's a negative case, wait till all labels are tried before adding to filtered
+        if not is_in and not match_found:
+            filtered.append(image_info)
 
-        return filtered, dupe_count
+    return filtered, dupe_count
 
+import operator
+
+def count_labels(image_infos):
+    label_counts = dict()
+
+    for im in image_infos:
+        label_infos = im[-1]
+        for label_info in label_infos:
+            label = label_info[0]
+            if label in label_counts:
+                label_counts[label] = label_counts[label] + 1
+            else:
+                label_counts[label] = 1
+
+    return label_counts
+
+
+def filter_files(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
     cvat_files = []
     if os.path.isdir(path):
         cvat_files = glob_files(path, file_type='*.xml')
@@ -596,50 +643,44 @@ def filter_files(path, from_format, to_format=LabelFormat.EDGE_IMPULSE):
         parsed.extend(p)
 
     # filtered = parsed
-    # labels_to_filter = ["warning@Engine"]
-    # labels_to_filter = ["alert@Steering"] - 21
-    # labels_to_filter = ["warning@Parking"] - 84
-    # labels_to_filter = ["warning@Brake"] - 206
-    # labels_to_filter = ["warning@StabilityOff"] - 228
-    # labels_to_filter = ["warning@StabilityOn"] - 273
-    # labels_to_filter = ["warning@Tire"] - 345
-    # labels_to_filter = ["alert@Brake"] - 468
     filtered, dupe_count = filter_by_labels(parsed, TO_EXCLUDE, is_in=False)
     # filtered, dupe_count = filter_by_labels(filtered, labels_to_filter)
     print(len(filtered), "dupe:", dupe_count)
-    #
-    # if to_format == LabelFormat.EDGE_IMPULSE:
-    #     convertor = EdgeImpulseConverter()
-    # elif to_format == LabelFormat.YOLOV5:
-    #     convertor = YoloV5Converter()
-    # elif to_format == LabelFormat.CVAT_XML:
-    #     convertor = CVATXmlConverter()
-    # else:
-    #     print('Unsupported output format {}'.format(to_format))
-    #
-    # print("Found {}".format(len(filtered)))
-    #
-    # random.shuffle(filtered)
-    #
-    # folder_prefix = "test10"
-    # # Write 100 by
-    # from_id = 0
-    # to_id = 400
-    # for folder_id in range(1):
-    #     chunk = filtered[from_id:to_id]
-    #     print("Chunk is ", len(chunk))
-    #
-    #     path_out = os.path.join(os.path.dirname(path),
-    #                             "{}_{}.xml".format(folder_prefix, folder_id))
-    #
-    #     project_name = "{}_{}".format(folder_prefix, folder_id)
-    #     convertor.write(project_name, chunk, path_out)
-    #
-    #     # move all data files
-    #     move_data_files(os.path.dirname(path), project_name, chunk)
-    #
-    #     from_id = to_id + 1
-    #     to_id = from_id + 100
+
+    if to_format == LabelFormat.EDGE_IMPULSE:
+        convertor = EdgeImpulseConverter()
+    elif to_format == LabelFormat.YOLOV5:
+        convertor = YoloV5Converter()
+    elif to_format == LabelFormat.CVAT_XML:
+        convertor = CVATXmlConverter()
+    else:
+        print('Unsupported output format {}'.format(to_format))
+
+    print("Found {}".format(len(filtered)))
+
+    random.shuffle(filtered)
+
+    folder_prefix = "train_top15"
+    # Write 100 by
+    from_id = 0
+    to_id = 100
+    for folder_id in range(10):
+        chunk = filtered[from_id:to_id]
+        print("Chunk is ", len(chunk))
+        print(sorted(count_labels(chunk).items()))
+
+        path_out = os.path.join(os.path.dirname(path),
+                                "{}_{}.xml".format(folder_prefix, folder_id))
+
+        project_name = "{}_{}".format(folder_prefix, folder_id)
+        # convertor.write(project_name, chunk, path_out)
+        convertor.write(project_name, chunk, path_out)
+
+        # move all data files
+        move_data_files(os.path.dirname(path), project_name, chunk)
+
+        from_id = to_id + 1
+        to_id = from_id + 100
 
 
 def move_data_files(parent_folder, folder_to, chunk):
@@ -684,6 +725,7 @@ def move_data_files(parent_folder, folder_to, chunk):
             exit(-1)
 
     print("Moved ", moved_count)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
